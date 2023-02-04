a5d5373ed68662ee2ed81ddaf325bc8d1c266ff5
ripaelit <ripaelit1111@gmail.com>
2023-02-04 23:21:28 +0800

    Initial commit

---
+++ constants/index.ts
@@ -0,0 +1,7 @@
+import {BigNumber} from 'ethers';
+import {parseEther} from 'ethers/lib/utils';
+
+const contractConstants = {
+};
+
+export default contractConstants as Readonly<typeof contractConstants>;
