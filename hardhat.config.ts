6c8f6991dd1b40180573a3ebb336e9e2da55d482
ripaelit <ripaelit1111@gmail.com>
2023-02-07 20:37:35 +0800

    Update hardhat config to prevent deploy error (gas limitation)

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -12,14 +12,17 @@ import '@nomicfoundation/hardhat-chai-matchers';
 const config: HardhatUserConfig = {
   defaultNetwork: 'hardhat',
   solidity: {
-    compilers: [{version: '0.8.9', settings: {}}],
+    compilers: [{version: '0.8.9', settings: {optimizer: {enabled:true, runs:200}}}],
   },
   // redirect typechain output for the frontend
   typechain: {
     outDir: './types/typechain',
   },
   networks: {
-    hardhat: {},
+    hardhat: {
+      gas: 30000000,
+      gasPrice: 8000000000
+    },
     localhost: {},
   },
 };
