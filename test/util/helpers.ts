c30a1cbda36c7b9571c7ef182215dbe3a08e2bba
ripaelit <ripaelit1111@gmail.com>
2023-02-19 09:36:03 +0800

    Removed helpers.ts(unused)

--- test/util/helpers.ts
+++
@@ -1,6 +0,0 @@
-import { ethers } from 'hardhat';
-
-export const deployNew = async (contractName: string, params: any[] = []) => {
-  const C = await ethers.getContractFactory(contractName);
-  return await C.deploy(...params)
-}
