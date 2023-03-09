1b029bfeefde16ee8c798f60a5ca8b538d87fad3
ripaelit <ripaelit1111@gmail.com>
2023-03-09 12:50:51 +0800

    removed console.sol

--- contracts/mozaic/StargateDriver.sol
+++ contracts/mozaic/StargateDriver.sol
@@ -8,8 +8,6 @@ import "./ProtocolDriver.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
-import "hardhat/console.sol";
-
 contract StargateDriver is ProtocolDriver{
     using SafeMath for uint256;
 
