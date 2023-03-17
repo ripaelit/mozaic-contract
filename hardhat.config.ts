f32981ded6cd272ac8a5d5056b906a0337762939
ripaelit <ripaelit1111@gmail.com>
2023-03-17 10:22:54 +0800

    udpate hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -19,9 +19,9 @@ const { mnemonic, bscscanApiKey, goerliApiKey, fantomApiKey } = require('./secre
 const ALCHEMY_API_KEY = "bnbMl8TQlq10-LjtuZJNae_egNyt36Ye";
 
 
-const GOERLI_PRIVATE_KEY_1 = "694602b4c1ec4c15e43b8fb7d897fe387536f93a771b9f33c5b01deab76623c9";    // acount1
-const GOERLI_PRIVATE_KEY_2 = "3bcdb1523b4dae87e050231735b6c7f0464ef65b8487f61092b8fe6c5fb59f6a";    // acount2
-const GOERLI_PRIVATE_KEY_3 = "c29075ed81f5cec37138e3474e90613ba5f1f2e97ee55824aa7a0702242a3711";    // acount3
+const PRIVATE_KEY_1 = "0x694602b4c1ec4c15e43b8fb7d897fe387536f93a771b9f33c5b01deab76623c9";    // acount1
+const PRIVATE_KEY_2 = "0x3bcdb1523b4dae87e050231735b6c7f0464ef65b8487f61092b8fe6c5fb59f6a";    // acount2
+const PRIVATE_KEY_3 = "0xc29075ed81f5cec37138e3474e90613ba5f1f2e97ee55824aa7a0702242a3711";    // acount3
 
 const INFURA_ID = "6e9457c7d3754cc2a205c109a69e516f";
 
@@ -64,19 +64,19 @@ const config: HardhatUserConfig = {
             gas: 30000000, //"auto", // 30000000
             gasPrice: 20000000000,
         },
-        goerli: {
-            // url: "https://goerli.blockpi.network/v1/rpc/public",
-            // url: "https://eth-goerli.g.alchemy.com/v2/" + ALCHEMY_API_KEY,
-            // url: `https://eth-goerli.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
-            // url: `https://eth-goerli.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
-            url: "https://goerli.infura.io/v3/" + INFURA_ID,
-            // url: "https://eth-goerli.public.blastapi.io",
-            chainId: 5,
-            gasPrice: 20000000000,
-            // accounts: [GOERLI_PRIVATE_KEY_1],
-            // accounts: [GOERLI_PRIVATE_KEY_1, GOERLI_PRIVATE_KEY_2, GOERLI_PRIVATE_KEY_3],
-            accounts: {mnemonic: mnemonic},
-        },
+        // goerli: {
+        //     // url: "https://goerli.blockpi.network/v1/rpc/public",
+        //     // url: "https://eth-goerli.g.alchemy.com/v2/" + ALCHEMY_API_KEY,
+        //     // url: `https://eth-goerli.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
+        //     // url: `https://eth-goerli.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
+        //     url: "https://goerli.infura.io/v3/" + INFURA_ID,
+        //     // url: "https://eth-goerli.public.blastapi.io",
+        //     chainId: 5,
+        //     gasPrice: 20000000000,
+        //     // accounts: [PRIVATE_KEY_1],
+        //     // accounts: [PRIVATE_KEY_1, PRIVATE_KEY_2, PRIVATE_KEY_3],
+        //     accounts: {mnemonic: mnemonic},
+        // },
         bsctest: {
             url: `https://data-seed-prebsc-1-s3.binance.org:8545/`,
             chainId: 97,
@@ -97,8 +97,8 @@ const config: HardhatUserConfig = {
         // Obtain one at https://etherscan.io/
 
         // apiKey: goerliApiKey,
-        // apiKey: bscscanApiKey,
-        apiKey: fantomApiKey,
+        apiKey: bscscanApiKey,
+        // apiKey: fantomApiKey,
     },
     paths: {
         sources: "./contracts",
