bed93472b64e5a640202b2272fa359c3e8b09d1e
ripaelit <ripaelit1111@gmail.com>
2023-03-22 09:47:16 +0800

    give and refund balance script

--- scripts/test/mytest.ts
+++ scripts/test/mytest.ts
@@ -2,14 +2,16 @@ import { ethers } from 'hardhat';
 import { returnBalance, returnBalanceFrom, sendBalance } from '../util/testUtils';
 
 async function main() {
-    // await returnBalanceFrom([
-    //     "0x2505F9e165bB950089B7725471Fc9373Ac85F6c7",
-    //     "0x605A6498E75Abc7Da4A2219a1853CC5541DAFf7E",
-    // ]);
+    await returnBalanceFrom([
+        "0x1B7F72a89447486Af0e25E8545F04b70ea254DBa",
+        "0x4847072f62AeFa4F8D3dA9634aF9A91e585d391b",
+    ]);
+
     await sendBalance([
-        ethers.utils.parseEther("5"),
+        ethers.utils.parseEther("2"),
         ethers.utils.parseEther("300"),
     ]);
+
     // await returnBalance();
 }
   
