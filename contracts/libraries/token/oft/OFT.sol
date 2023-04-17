34fab851b4cf9b2fe7948b3b9e85d0eb159326dc
ripaelit <ripaelit1111@gmail.com>
2023-04-17 23:54:50 +0800

    update import path

--- contracts/libraries/token/oft/OFT.sol
+++ contracts/libraries/token/oft/OFT.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/utils/introspection/IERC165.sol";
-import "../../interfaces/IOFT.sol";
+import "../../../interfaces/IOFT.sol";
 import "./OFTCore.sol";
 
 // override decimal() function is needed
--- contracts/libraries/token/oft/OFTCore.sol
+++ contracts/libraries/token/oft/OFTCore.sol
@@ -2,8 +2,8 @@
 
 pragma solidity ^0.8.0;
 
-import "../lzApp/NonblockingLzApp.sol";
-import "../../interfaces/IOFTCore.sol";
+import "../../lzApp/NonblockingLzApp.sol";
+import "../../../interfaces/IOFTCore.sol";
 import "@openzeppelin/contracts/utils/introspection/ERC165.sol";
 
 abstract contract OFTCore is NonblockingLzApp, ERC165, IOFTCore {
