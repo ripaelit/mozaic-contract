6866aa9e5c920f509a7c7562feb0f7359461ee09
ripaelit <ripaelit1111@gmail.com>
2023-04-17 23:53:33 +0800

    update import path

--- contracts/mozaic/MozaicLP.sol
+++ contracts/mozaic/MozaicLP.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.9;
 
 // imports
 import "../interfaces/IOFT.sol";
-import "../libraries/oft/OFTCore.sol";
+import "../libraries/token/oft/OFTCore.sol";
 
 // libraries
 import "@openzeppelin/contracts/access/Ownable.sol";
